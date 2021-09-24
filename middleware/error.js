4f6c545d6482f7edef7f4a7062d6059603f0fcb3
reagan-mybeatus <reagan.ekhameye@mybeatus.com>
2021-09-24 13:16:53 +0100

    refactor: modify error messages

--- middleware/error.js
+++ middleware/error.js
@@ -1,3 +1,5 @@
+const ErrorResponse = require('../utils/errorResponse')
+
 const errorHandler = (err, req, res, next) => {
 	let error = {
 		...err,
@@ -7,6 +9,13 @@ const errorHandler = (err, req, res, next) => {
 
 	// console.log(err.stack.red);
 	console.log(err)
+
+	// Not Found
+	if (err.response.data.detail === 'Not found') {
+		// const message = `Resource (${err.response.config.url}) not found`
+		const message = `Resource not found`
+		error = new ErrorResponse(message, 404)
+	}
 
 	res.status(error.statusCode || 500).json({
 		success: false,
